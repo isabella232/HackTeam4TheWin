9dd135692cf61100f3161769251924f28c9a6207
Jeff Valore <rally25rs@yahoo.com>
2013-11-07 14:14:29 -0500

    display current time in offices

---
+++ content/js/clocks.js
@@ -0,0 +1,52 @@
+(function ($, kendo, moment) {
+	"use strict";
+
+	var officeData = new kendo.data.DataSource({
+		transport: {
+			read: "data/offices.json"
+		}
+	});
+
+	var timelineData = new kendo.data.DataSource({
+		data: []
+	});
+
+	var generateTimelineData = function () {
+		var data = [];
+		var offices = officeData.view();
+		var numOffices = offices.length;
+		var office;
+		var momentForDay;
+
+		for(var i = 0; i < numOffices; i++) {
+			office = offices[i];
+			momentForDay = moment(this.get("selectedTime")).tz(timezone).add("days", -8);
+
+			for(var dayIdx = -7; dayIdx <= 30; dayIdx++) {
+				momentForDay.add("days", 1);
+				data.push({
+					name: office.name,
+					day: dayIdx,
+					offset: momentForDay.zone() / 60
+				});
+			}
+		}
+
+		timelineData.data(data);
+	};
+
+	var viewModel = kendo.observable({
+		selectedTime: moment().toDate(),
+		offices: officeData,
+
+		calcTime: function (data) {
+			var timezone = data.get("timezone");
+			if(timezone) {
+				return moment(this.get("selectedTime")).tz(timezone).format("H:mm a z");
+			}
+			return "";
+		}
+	});
+
+	kendo.bind($("body"), viewModel);
+})(window.jQuery, window.kendo, window.moment);
